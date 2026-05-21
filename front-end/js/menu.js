eef11b2bdcba3d77a36e1313bf3e3aba7e35043a
felipelima2005 <felipelimaoliveira2008@gmail.com>
2026-05-21 11:52:58 -0300

    feat: aplicação de js no menu concluida e ajuste nas estruturas das páginas

--- front-end/js/menu.js
+++ front-end/js/menu.js
@@ -1,18 +1,53 @@
-const menuItens = document.querySelector(".cabecalho__item")
 
-menuItens.addEventListener("mouseenter", () => {
+const cabecalho = document.querySelector('.cabecalho');
 
-    menuItens.style.transform = "translateY(-7px)";
 
+cabecalho.addEventListener('mouseenter', () => {
+    
+    cabecalho.style.boxShadow = '0px 4px 15px rgba(115, 228, 63, 0.87)';
+    cabecalho.style.transition = '0.4s';
 });
 
-menuItens.addEventListener("mouseleave", () => {
 
-    menuItens.style.transform = "translateY(0)";
-
+cabecalho.addEventListener('mouseleave', () => {
+    
+    cabecalho.style.boxShadow = 'none';
+    cabecalho.style.transition = '0.4s';
 });
-menuItens.addEventListener("click", () => {
 
-    menuItens.classList.toggle("ativo");
 
+
+const menuItems = document.querySelectorAll('.cabecalho__item');
+
+menuItems.forEach(item => {
+
+
+    item.addEventListener('mouseenter', () => {
+        item.style.borderBottom = '3px solid var(--secondary)';
+        item.style.paddingBottom = '5px';
+        item.style.transition = '0.3s'; 
+    });
+
+    item.addEventListener('mouseleave', () => {
+        item.style.borderBottom = '3px solid var(--secondary)';
+        item.style.paddingBottom = '0px';
+        item.style.borderBottom = '2px solid transparent';
+    });
 });
+
+const menuContato = document.querySelector('.cabecalho__item__contato')
+
+cabecalho.addEventListener('mouseenter',() =>{
+        menuContato.style.borderBottom = '3px solid var(--secondary)';
+        menuContato.style.paddingBottom = '5px';
+        menuContato.style.transition = '0.3s'; 
+        menuContato.style.color = 'var(--bg-primary)'
+
+})
+menuContato.addEventListener('mouseleave', () => {
+        menuContato.style.borderBottom = '3px solid var(--secondary)';
+        menuContato.style.paddingBottom = '0px';
+        menuContato.style.borderBottom = '2px solid transparent';
+        menuContato.style.color = 'var(--text-main)'
+    });
+
